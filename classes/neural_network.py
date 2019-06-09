4b168156b7589d9ecf1a337875abf0d8a1bdff3c
G. Puetz <cobragp@hotmail.com>
2019-06-09 15:28:33 +0200

    Eliminated internal storage and changed forward function to take input

--- classes/neural_network.py
+++ classes/neural_network.py
@@ -1,41 +1,38 @@
 import numpy as np
 
+
 class NeuralNetwork:
 
-    def __init__(self, input_size, output_size, hidden_layer_size):
+    def __init__(self, input_size, output_size, hidden_layer_sizes):
         """Create a neural network with input_size input neurons,
         output_size output neurons and one hidden layer with
         hidden_layer_size neurons in it. Weights are initialized
-        randomly, the neurons all start with 0"""
-        self.inputs = np.zeros(input_size)
-        self.outputs = np.zeros(output_size)
-        self.out_pre_activation = np.zeros(output_size)
-        self.hidden_layer = np.zeros(hidden_layer_size)
-        self.hl_pre_activation = np.zeros(hidden_layer_size)
-        self.weights = [np.random.rand(input_size, hidden_layer_size),
-                        np.random.rand(hidden_layer_size, output_size)]
-        self.num_connections = [input_size * hidden_layer_size,
-                                hidden_layer_size * output_size]
-        self.weight_shapes = [w.shape for w in self.weights]
+        randomly, the neurons all start with 0
+        """
+        self.layer_sizes = [input_size] + hidden_layer_sizes + [output_size]
+        self.weights = [np.random.rand(prev_layer, next_layer)
+                        for prev_layer, next_layer
+                        in zip(self.layer_sizes, self.layer_sizes[1:])]
+        self._num_connections = [prev_layer_size * next_layer_size
+                                for prev_layer_size, next_layer_size
+                                in zip(self.layer_sizes, self.layer_sizes[1:])]
+        self._weight_shapes = [w.shape for w in self.weights]
 
     @staticmethod
     def activation_function(x):
         """Activation function for the neurons in this neural network
-        Currently: Sigmoid function"""
+        Currently: Sigmoid function
+        """
         return 1/(1+np.exp(-x))
 
-    def set_inputs(self, inputs):
-        """Set the input neurons. Inputs must be a list of numpy array
-        with as many entries as there are input neurons"""
-        self.inputs = np.array(inputs)
-
-    def forward(self):
+    def forward(self, inputs):
         """Compute the values of the hidden layer and the output layer for
-        the previously set inputs and the current value of the weights"""
-        self.hl_pre_activation = np.dot(self.inputs, self.weights[0])
-        self.hidden_layer = self.activation_function(self.hl_pre_activation)
-        self.out_pre_activation = np.dot(self.hidden_layer, self.weights[1])
-        self.outputs = self.activation_function(self.out_pre_activation)
+        the previously set inputs and the current value of the weights
+        """
+        neurons = inputs
+        for weights in self.weights:
+            neurons = self.activation_function(np.dot(neurons,weights))
+        return neurons
 
     def get_weights(self):
         """Get a flattened array of the weights"""
@@ -43,9 +40,10 @@ class NeuralNetwork:
 
     def set_weights(self, new_weights):
         """Set the weights to the new provided values. The weights should
-        be given in form of a flat numpy array"""
+        be given in form of a flat numpy array
+        """
         start = 0
         self.weights = []
-        for (n, shape) in zip(self.num_connections,self.weight_shapes):
-            self.weights.append(np.reshape(new_weights[start:start+n],shape))
+        for (n, shape) in zip(self._num_connections, self._weight_shapes):
+            self.weights.append(np.reshape(new_weights[start:start+n], shape))
             start += n
--- tests/test_neural_network.py
+++ tests/test_neural_network.py
@@ -4,21 +4,17 @@ import numpy as np
 class Test:
 
     def test_setting_and_getting_weights(self):
-        nn = NeuralNetwork(2,1,3)
+        nn = NeuralNetwork(2,1,[3])
         weights = nn.get_weights()
-        print(nn.weights)
-        print(weights)
         nn.set_weights(weights)
         new_weights = nn.get_weights()
         np.testing.assert_almost_equal(weights,new_weights)
 
     def test_forward(self):
-        nn = NeuralNetwork(2,1,3)
+        nn = NeuralNetwork(2,1,[3])
         weights = [np.array([[0.1,0.3,0.6],[0.2,0.5,0.7]]),
                    np.array([[0.8],[0.9],[1.1]])]
         weights = np.concatenate([ws.flatten() for ws in weights])
         nn.set_weights(weights)
-        nn.set_inputs(np.array([0.6,0.3]))
-        nn.forward()
-        output = nn.outputs[0]
-        np.testing.assert_almost_equal(output, 0.838917292)
+        outputs = nn.forward(np.array([0.6,0.3]))
+        np.testing.assert_almost_equal(outputs[0], 0.838917292)
